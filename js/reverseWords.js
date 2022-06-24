9d36d5df09826932b32fe014b31549eb47efa154
Leighton <LeightonShervington@gmail.com>
2022-06-24 19:39:26 -0400

    Refactored to be more concise

--- js/reverseWords.js
+++ js/reverseWords.js
@@ -1,7 +1,16 @@
 // Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
+
+// original version
 function reverseWords(str) {
     str = str.split(' ')
     let reversedWords = str.map( (word, i) => word.split('').reverse().join('') )
     return reversedWords.join(' ')
+  }
+
+  //refactored concise version
+  function reverseWords(str) {
+    str = str.split(' ')
+    
+    return str.map( (word, i) => word.split('').reverse().join('') ).join(' ')
   }
